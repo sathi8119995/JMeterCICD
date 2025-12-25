8977cdad6a1d9411df9de1d81e630b8bc8785731
Sathi Nag <sathi.nag8@gmail.com>
2025-12-25 16:03:04 +0530

    Refactor code structure for improved readability and maintainability

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -13,7 +13,7 @@ pipeline {
             steps {
                 // TODO: Update 'url' to your actual GitHub repository URL
                 git branch: 'main',
-                    url: 'https://github.com/your-username/jmeter-ci-cd.git'
+                    url: 'https://github.com/sathi8119995/JMeterCICD.git'
             }
         }
 
